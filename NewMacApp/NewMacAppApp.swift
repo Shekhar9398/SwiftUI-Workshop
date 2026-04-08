38f4b30889c64a98913825082758096938bc097e
Shekhar Manwar <shekhar.manwar@thinkitive.com>
2026-04-08 17:50:45 +0530

    Initial Commit

---
+++ NewMacApp/NewMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewMacAppApp.swift
+//  NewMacApp
+//
+//  Created by Mac on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NewMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
